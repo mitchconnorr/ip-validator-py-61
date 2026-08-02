05b301dd4232706c3488fec937199bccaf65dfdb
mitchconnorr <42@felix-au.me>
2026-08-02 08:48:19 +0530

    Update validator.py - Step 7

--- validator.py
+++ validator.py
@@ -2,3 +2,8 @@ import socket
 def validate(addr):
     for fam in (socket.AF_INET, socket.AF_INET6):
         try:
+            socket.inet_pton(fam, addr)
+            return True
+        except socket.error:
+            pass
+    return False
